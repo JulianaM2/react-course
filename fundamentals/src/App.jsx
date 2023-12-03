91c23efcdba85276b1efe31838ea00b47486f56d
Juliana Muñoz Marín <jmmalcg@gmail.com>
2023-12-03 15:40:43 -0500

    Add counter to use useState

--- fundamentals/src/App.jsx
+++ fundamentals/src/App.jsx
@@ -1,7 +1,22 @@
+import './App.css'
+import { useState } from 'react';
+
 const App = () => {
-  return <div className="container">
-    <h1>Hello world!</h1>
-  </div>
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => setCount(count + 1);
+  const handleRemove = () => setCount(count - 1);
+  const handleReset = () => setCount(0);
+
+  return <>
+    <div className="container">
+      <h1>Counter: {count}</h1>
+      <hr />
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleRemove}>-</button>
+    </div>
+  </>
 }
 
 export default App;
