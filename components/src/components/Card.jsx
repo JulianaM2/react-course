39fcde40ba659dcfb1d19c46729da00e3cc9d1c2
Juliana Muñoz Marín <jmmalcg@gmail.com>
2023-12-04 13:08:08 -0500

    Add card component

---
+++ components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.css"
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.svg" />
+      <h3>Python</h3>
+    </div>
+  )
+};
+
+export default Card;
